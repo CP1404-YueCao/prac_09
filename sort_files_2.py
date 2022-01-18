c85f819a0323ee7ef711770a326cb0c1be96ce9c
Yue Cao <yue.cao@my.jcu.edu.au>
2022-01-18 16:50:12 +0800

    Ask the user for what category would they like to sort files into

--- sort_files_2.py
+++ sort_files_2.py
@@ -3,12 +3,17 @@ import os
 
 
 def main():
-    """Move the files into the folder with the same name"""
+    """Move the files into the folder where the user wants to store files"""
     os.chdir("FilesToSort")
+    extension_of_category = {}
     for filename in os.listdir('.'):
         if os.path.isdir(filename):
             continue
+
         extension = filename.split('.')[-1]
+        if extension not in extension_of_category:
+            category = input(f"What category would you like yo sort {extension} files into? ")
+            extension_of_category[extension] = category
         try:
             os.mkdir(extension)
         except FileExistsError:
